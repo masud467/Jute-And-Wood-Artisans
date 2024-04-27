2819807d1372f69faefa4a275c8909f96fad31a0
masud4657 <masudpfrnt@gmail.com>
2024-04-28 00:18:34 +0600

    added the banner section of Home page section.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,75 @@
-
-
 const Home = () => {
-    return (
-        <div>
-          <div>
-          <h1>Slider/carousal is coming...</h1>
+  return (
+    <div>
+      <div>
+        <div className="carousel w-full">
+          <div id="slide1" className="carousel-item relative w-full">
+            <img
+              src="https://i.ibb.co/qFv1qCH/room-interior-design.jpg"
+              className="w-full rounded-lg"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide4" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide2" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
           </div>
-          <div>
-            <h1> Craft items section </h1>
+          <div id="slide2" className="carousel-item relative w-full">
+            <img
+              src="https://i.ibb.co/Dtqfckc/handcrafted-wooden-decorative-vase.jpg"
+              className="w-full rounded-lg"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide1" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide3" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
           </div>
-          <div>
-          Art & Craft Categories Section for challenging part
+          <div id="slide3" className="carousel-item relative w-full">
+            <img
+              src="https://i.ibb.co/s6zQxsD/interior-decoration-inspired-by-mexican-folklore.jpg"
+              className="w-full rounded-lg"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide2" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide4" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
           </div>
-          <div>
-            <h1>2 Extra Sections</h1>
+          <div id="slide4" className="carousel-item relative w-full">
+            <img
+              src="https://i.ibb.co/3svQNdp/view-indoor-space-ready-comfortable-activities-1.jpg"
+              className="w-full rounded-lg"
+            />
+            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+              <a href="#slide3" className="btn btn-circle">
+                ❮
+              </a>
+              <a href="#slide1" className="btn btn-circle">
+                ❯
+              </a>
+            </div>
           </div>
         </div>
-    );
+      </div>
+      <div>
+        <h1> Craft items section </h1>
+      </div>
+      <div>Art & Craft Categories Section for challenging part</div>
+      <div>
+        <h1>2 Extra Sections</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
